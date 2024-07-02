21dfa54fb19b86b211fd2c97e51c860dedfb24ca
Corrigan <corrigannewsome@gmail.com>
2024-07-01 21:10:01 -0400

    added reading/writitng file, and finished the basic loops lesson

---
+++ Python_101/loops.py
@@ -0,0 +1,17 @@
+a = 0
+print('----')
+while a < 5:
+    a+= 1
+    print(a)
+
+print('----')
+for i in [0, 1, 2, 3, 4, 5]:
+    print(i)
+    
+print('----')
+for i in range(5):
+    print(i + 6)
+    
+print('----')
+for c in "string cheese":
+    print(c)
