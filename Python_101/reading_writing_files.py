a8a2089606b98f655d1206fc8e46e068c780d70e
Corrigan <corrigannewsome@gmail.com>
2024-07-03 16:50:29 -0400

    finished user input lesson

--- Python_101/reading_writing_files.py
+++ Python_101/reading_writing_files.py
@@ -1,13 +1,20 @@
+#Need to put in full path to file to open. 
 f = open('/home/anubis/Desktop/Tech/test.txt', 'rt')
 print(f.readlines())
 
-f.seek(0)
+f.seek(0)#changes file position
 print('----for loop---')
 for line in f:
     print(line.strip())
     
 f.close()
 
+#write mode
+f = open('/home/anubis/Desktop/Tech/test01.txt', 'w')
+f.write('test line\ntest line two\n')
+f.close()
+
+# append mode
 f = open('/home/anubis/Desktop/Tech/test01.txt', 'w')
 f.write('test line\ntest line two\n')
 f.close()
---
+++ Python_101/user_input.py
@@ -0,0 +1,10 @@
+# test = input("Enter the IP: ")
+# print(test)
+
+while True:
+    test = input('Enter the IP: ')
+    print('>>>{}'.format(test))
+    if test == 'exit':
+        break
+    else:
+        print('exploitting...')
