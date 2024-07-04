92da5d83b205bf0c0f77bf533bae1c14c83ec8eb
Corrigan <corrigannewsome@gmail.com>
2024-07-04 11:49:34 -0400

    finished e_e_h and added a comprehensions.py file

--- Python_101/exceptions_error_handling.py
+++ Python_101/exceptions_error_handling.py
@@ -6,3 +6,15 @@ except Exception as e:
     print(e)
 finally:
     print('this message')
+    
+    n = 100
+    if n == 0:
+        raise Exception ("n can't be 0!")
+    if type(n) is not int:
+        raise Exception("n must be an int!")
+    
+print(1/n)
+
+n = 0
+assert(n != 0)
+print(1/n)
