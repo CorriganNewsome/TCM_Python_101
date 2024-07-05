15ef1c0d46c797a849df4c9638f7300421b7e082
Corrigan <corrigannewsome@gmail.com>
2024-07-05 15:26:33 -0400

    finished comprehensions lesson

---
+++ Python_101/comprehensions.py
@@ -0,0 +1,11 @@
+list1 = ['a', 'b', 'c']
+print(list1)
+
+list2 = [x for x in list1]
+print(list2)
+
+list3 = [x for x in list1 if x == 'a']
+print(list3)
+
+list4 = [x for x in range(5)]
+print(list4)
